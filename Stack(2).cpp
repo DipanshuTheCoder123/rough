40ba7ccf6cec8884ee21064c6d6ec88a67c9a4c3
Dipanshu Dhawade <dipanshudhawade60@gmail.com>
2026-03-18 09:51:20 +0530

    Add files via upload

---
+++ Stack(2).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+ int main()
+ {
+  int n = 123;
+      string s =  to_string(n);
+      cout  << s.size()<<endl;
+      int  num =  stoi(s);
+       cout  << num;
+ }
+
+
