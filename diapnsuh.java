f5114db497739c3052603b4a803cdc47a33c94fa
Dipanshu Dhawade <dipanshudhawade60@gmail.com>
2026-03-24 16:09:14 +0530

    Add files via upload

---
+++ diapnsuh.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class diapnsuh {
+
+    
+    public static void main(String[] args) {
+        
+         double pow = Math.pow(12, 3);
+                   System.out.println(pow);
+
+         int num=10;
+         String num2="10";
+        
+         if (num==Integer.parseInt(num2)) {
+            System.out.println(true);
+         }
+
+         System.out.println("1"+"5");
+        // for(int val:arr){
+        //     sys
+        // }
+        ArrayList list=new ArrayList<>();
+        list.add(1);
+        list.add(12);
+        list.add(33);
+
+        System.out.println(list);
+
+        int []arr={7,89,90};
+        int [] arrnew = Arrays.copyOf(arr,3);
+          int r[]=  Arrays.copyOf(arr,arr.length);
+
+          int arr1232[][]={{1,2},{1,23}};
+          arr1232.fla
+    } 
+}
